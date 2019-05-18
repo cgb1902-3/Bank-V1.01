04ca19b471199d6d09cc41e7b6e9d52808c179ab
wy <1438015228@qq.com>
2019-05-18 15:15:20 +0800

    Bank

--- BankV1.01/src/main/java/com/bank/page/controller/PageController.java
+++ BankV1.01/src/main/java/com/bank/page/controller/PageController.java
@@ -1,4 +1,4 @@
-package com.bank.sys.controller;
+package com.bank.page.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
