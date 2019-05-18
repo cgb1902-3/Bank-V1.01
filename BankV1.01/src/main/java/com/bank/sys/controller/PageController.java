e5eb47baec7527edbacfbe78ad1cb060e4bd6059
wy <1438015228@qq.com>
2019-05-18 11:46:04 +0800

    Bank

--- BankV1.01/src/main/java/com/bank/sys/controller/PageController.java
+++ BankV1.01/src/main/java/com/bank/sys/controller/PageController.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PageController {
-	@RequestMapping("test")
+	@RequestMapping("doIndex")
 	public String test(){
-		return "Test";
+		return "index";
 	}
 }
